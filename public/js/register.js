c8a8c2138b72b4191cbac363f332785d09241a28
Sean Charboneau <Sean Charboneau>
2016-09-12 15:13:04 -0500

    currency and bower

--- public/js/register.js
+++ public/js/register.js
@@ -1,21 +1,25 @@
 var RegisterViewModel = function() {
     var self = this;
 
-    self.register = function (e) {
-        var username = $('#username').val();
-        var password = $('#password').val();
-        var email = $('#email').val();
-        var firstName = $('#firstName').val();
-        var lastName = $('#lastName').val();
+    self.selectedCurrency = ko.observable();
+    self.username = ko.observable();
+    self.password = ko.observable();
+    self.email = ko.observable();
+    self.firstName = ko.observable();
+    self.lastName = ko.observable();
+
+    self.register = function () {
+        console.log(self.selectedCurrency());
         $.ajax({
             type: 'POST',
             url: '/register',
             data: {
-                username: username,
-                password: password,
-                email: email,
-                firstName: firstName,
-                lastName: lastName
+                username: self.username(),
+                password: self.password(),
+                email: self.email(),
+                firstName: self.firstName(),
+                lastName: self.lastName(),
+                baseCurrency: self.selectedCurrency().code
             },
             success: function(data) {
                 if(!data || data.error) {
